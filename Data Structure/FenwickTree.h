fdbd910f8d2a6c5ebbf2379caa8071de45d8bed9
Niloy Das <niloyokasd@gmail.com>
2024-11-04 03:09:17 +0600

    All hail lord Claude

--- Data Structure/FenwickTree.h
+++ Data Structure/FenwickTree.h
@@ -1,29 +1,35 @@
- * Description: Computes partial sums a[0] + a[1] + ... + a[pos - 1], and updates single elements a[i],
+/* * Description: Computes partial sums a[1] + a[2] + ... + a[pos], and updates single elements a[i],
  * taking the difference between the old and new value.
  * Time: Both operations are $O(\log N)$.
  * Status: Stress-tested
+ * Note: 1-based indexing
  */
 #pragma once
 
 struct FT {
-	vector<ll> s;
-	FT(int n) : s(n) {}
-	void update(int pos, ll dif) { // a[pos] += dif
-		for (; pos < sz(s); pos |= pos + 1) s[pos] += dif;
-	}
-	ll query(int pos) { // sum of values in [0, pos)
-		ll res = 0;
-		for (; pos > 0; pos &= pos - 1) res += s[pos-1];
-		return res;
-	}
-	int lower_bound(ll sum) {// min pos st sum of [0, pos] >= sum
-		// Returns n if no sum is >= sum, or -1 if empty sum is.
-		if (sum <= 0) return -1;
-		int pos = 0;
-		for (int pw = 1 << 25; pw; pw >>= 1) {
-			if (pos + pw <= sz(s) && s[pos + pw-1] < sum)
-				pos += pw, sum -= s[pos-1];
-		}
-		return pos;
-	}
+    vector<ll> s;
+    FT(int n) : s(n + 1) {} // size + 1 for 1-based indexing
+    
+    void update(int pos, ll dif) { // a[pos] += dif
+        for (; pos < sz(s); pos += pos & -pos) 
+            s[pos] += dif;
+    }
+    
+    ll query(int pos) { // sum of values in [1, pos]
+        ll res = 0;
+        for (; pos > 0; pos -= pos & -pos)
+            res += s[pos];
+        return res;
+    }
+    
+    int lower_bound(ll sum) { // min pos st sum of [1, pos] >= sum
+        // Returns n if no sum is >= sum, or 0 if empty sum is.
+        if (sum <= 0) return 0;
+        int pos = 0;
+        for (int pw = 1 << 25; pw; pw >>= 1) {
+            if (pos + pw < sz(s) && s[pos + pw] < sum)
+                pos += pw, sum -= s[pos];
+        }
+        return pos + 1;
+    }
 };
